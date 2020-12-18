50baa09adf17712fb75227228f5ba30a6e745fd5
Max <max@debian-fx>
2020-12-18 11:07:09 +0100

    Added tweaks, and readme in /root

---
+++ rootfs_overlay/etc/profile.d/aliases.sh
@@ -0,0 +1,5 @@
+alias ll="ls -lah"
+alias lS="ls -lahS"
+alias draw_crtc="luajit /usr/share/lua/5.1/libdrm/examples/draw_crtc.lua"
+alias drm_info="luajit /usr/share/lua/5.1/libdrm/examples/info.lua"
+alias drm_test="luajit /usr/share/lua/5.1/libdrm/examples/test.lua"
---
+++ rootfs_overlay/root/readme.txt
@@ -0,0 +1,1 @@
+Welcome to the bootmenu initramfs!
